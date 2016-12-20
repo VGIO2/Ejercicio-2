747df57e29cf85dae6a2421be44bfeb1a5e25304
Giovanna <orurog@gmail.com>
2016-12-20 10:15:26 -0500

    terminado

---
+++ main.js
@@ -0,0 +1,10 @@
+var numero1, numero2, numero3, promedio, resultado;
+
+numero1 = prompt ("Por favor, ingresa un número");
+numero2 = prompt ("Por favor, ingresa un segundo numero");
+numero3 = prompt ("Por favor, ingresa un tercer numero");
+
+promedio = parseFloat(numero1)+parseFloat(numero2)+parseFloat(numero3);
+resultado = promedio /3;
+
+alert(resultado);
